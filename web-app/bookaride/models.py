bbe28aa98fea4d4b3da2fe7fe11b77506f75059b
yxx137 <xy137@duke.edu>
2022-01-21 21:12:15 -0500

    remove my user

--- web-app/bookaride/models.py
+++ web-app/bookaride/models.py
@@ -1,11 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-
-class User(models.Model):
-    name = models.CharField(max_length = 20)
-    email = models.EmailField(max_length = 254, null = True)
 
 
 class Vehicle(models.Model):
