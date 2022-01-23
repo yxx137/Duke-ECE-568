0b3dde275689191e53c3d16989e64f596b8532f4
yxx137 <xy137@duke.edu>
2022-01-23 12:25:17 -0500

    change password

--- web-app/firsthomework/settings.py
+++ web-app/firsthomework/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '19990606',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': 5432,
     }
